5e686057af6e64522228fea193e3cdf215a260b2
student <daisychepkirui@gmail.com>
2024-07-25 08:17:59 +0300

    add file

--- api/serializer.py
+++ api/serializer.py
@@ -1,43 +1,29 @@
-# from rest_framework import serializer
-# from student.models import Student
-
-# class StudentSerializer(serializers,modelSerializer):
-#     class Meta:
-#         model =Student
-#         fields ="__all__"from rest_framework import serializers
+from rest_framework import serializers
 from student.models import Student
-
+from classperiod.models import ClassPeriod
+from teacher.models import Teacher
+from classes.models import Class
+from course.models import Course
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['id', 'name', 'email', 'grade']
-
-class StudentSerializer(serializers.ModelSerializer):
+        fields = "__all__"
+class ClassPeriodSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Student
+        model = ClassPeriod
         fields = "__all__"
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
         fields = "__all__"
+class ClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Class
+        fields = "__all__"
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
-class ClassRoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        model =ClassRoom
-        fields = "__all__"
-class PeriodSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Period
-        fields = '__all__'
-
-
-
-
-
-
 
 
 
--- api/urls.py
+++ api/urls.py
@@ -1,25 +1,34 @@
 # from django.urls import path
-
-# from .views import StudentListView
-
-# urlpatterns=[
-#     path("students/",StudentListView.as_view(),name= "student_list_view")
-# ]
 from django.urls import path
 from .views import StudentListView
-from.views import StudentDetailView
-
-rom django.urls import path
-from .views import PeriodListViews
-from .views import  CourseListViews
-from .views import ClassRoomListViews
-from .views import StudentListView
-from .views import TeacherListViews
-urlpatterns = [
-    path('student/', StudentListView.as_view(), name = 'student_list_view'),
-     path('Teacher/',TeacherListViews.as_view(),name = "teachers_list_view"),
-    path('Courses/',CourseListViews.as_view(),name = "course_list_view"),
-   path('ClassRoom/',ClassRoomListViews.as_view(),name = "class_room_list_view"),
- path('Period/',PeriodListViews.as_view(),name = "class_period_list_view"),
-path('student/<int:id>/',StudentDetailView.as_view(),name='student_list_view')
+from .views import CourseListView
+# from .views import ClassListView
+from .views import TeacherListView
+# from .views import ClassPeriodListView
+from .views import StudentDetailView
+# from .views import ClassDetailView
+# 
+from .views import TeacherDetailView
+from .views import CourseDetailView
+urlpatterns=[
+    # path("classperiods/",ClassPeriodListView.as_view(),name="classperiods_list_view"),
+    path("teacher/",TeacherListView.as_view(),name="teacher_list_view"),
+    path("student/",StudentListView.as_view(),name="student_list_view"),
+    # path("classes/",ClassListView.as_view(),name="classes_list_view"),
+    # path("course/",CourseListView.as_view(),name="course_list_view"),
+    # path("students/<int:id>/",StudentDetailView.afrom .views import ClassPeriodDetailViews_view(),name="student_detail_view"),
+    # path("classes/<int:id>/",ClassDetailView.as_view(),name="classes_detail_view"),
+    path("teacher/<int:id>/",TeacherDetailView.as_view(),name="teacher_detail_view"),
+    path("course/<int:id>/",CourseDetailView.as_view(),name="course_detail_view"),
+#     path("classperiod/<int:id>/",ClassPeriodDetailView.as_view(),name="classperiod_detail_view")
 ]
+
+
+
+
+
+
+
+
+
+
